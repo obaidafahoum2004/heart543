781c611ecd03e945b195218c1eacd1ed88f6b96d
obaida <obfahoum@gmail.com>
2023-03-21 10:04:24 +0200

    add user

--- app/src/main/java/com/example/heartreader/MainActivity.java
+++ app/src/main/java/com/example/heartreader/MainActivity.java
@@ -16,23 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView Useremail = (TextView) findViewById(R.id.Useremail);
-        TextView Password = (TextView) findViewById(R.id.password);
+        // TODO: add code the takes you to Login
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-        // admin & admin
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Useremail.getText().toString().equals("admin") && Password.getText().toString().equals("admin")) {
-                    // correct
-                    Toast.makeText(MainActivity.this, "LogIn Successful", Toast.LENGTH_SHORT).show();
-                } else {
-                    // incorrect
-                    Toast.makeText(MainActivity.this, "LogIn Failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
     }}
 
